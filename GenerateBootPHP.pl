3d224495e73fe1351f4faae7b1b1a79a7f8c6676
Andy Gill <andygill@ku.edu>
2008-09-04 13:25:00 -0500

    Adding tunnel and bootPHP generation.

---
+++ GenerateBootPHP.pl
@@ -0,0 +1,22 @@
+# usage: GenerateBootPHP [bootfile|mysql] <user> <db>
+open(PASS,"/Users/andy/private/db_pass");
+while(<PASS>) {
+    chop;
+    my ($DB_USER,$DB_HOST,$DB_NAME,$DB_PASSWORD) = split(/:/,$_);
+    if ($DB_USER eq $ARGV[1] && $DB_NAME eq $ARGV[2]) {
+	if ("bootfile" eq $ARGV[0]) {
+	    print "<?php\n";
+	    print "\$DB_USER = '$DB_USER';\n";
+	    print "\$DB_HOST = '$DB_HOST';\n";
+	    print "\$DB_NAME = '$DB_NAME';\n";
+	    print "\$DB_PASSWORD = '$DB_PASSWORD';\n";
+	    print "?>\n";
+	}
+	if ("mysql" eq $ARGV[0]) {
+	    if ($DB_PASSWORD ne "") {
+		print "-p'$DB_PASSWORD' ";
+	    }
+	    print "-u'$DB_USER' -h'$DB_HOST' '$DB_NAME' ";
+	}
+    }
+}
